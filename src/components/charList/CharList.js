4ebaabf2df1a923c0588a852fe540ce9df285ebb
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2025-01-15 17:04:37 +0300

    App and CharList was rewritten on hooks.

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+  import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import './charList.scss';
@@ -7,104 +7,67 @@ import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
-class CharList extends Component {
-  state = {
-    charList: [],
-    loading: true,
-    error: false,
-    newItemLoading: false,
-    offset: null,
-    charEnded: false,
-  };
+const CharList = (props) => {
 
-  marvelService = new MarvelService();
+  const [charList, setCharList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(null);
+  const [charEnded, setCharEnded] = useState(false);
 
-  componentDidMount() {
-    // const charListLocalState = localStorage.getItem('charList:state');
+  const marvelService = new MarvelService();  // объект, который конструируетсфя при помощи класса MarvelService
 
-    // if (!charListLocalState) {
-    //   localStorage.setItem('charList:state', JSON.stringify({
-    //     offset: 210
-    //   }))
-    // }
+  useEffect(() => {
+    onRequest();
+  }, [])  // 1 раз когда компонент был смонтирован вызываем onRequest (аналог componentDidMount)
 
-    // if (charListLocalState) {
-    //   const charListState = JSON.parse(charListLocalState);
-    //   this.setState({
-    //     offset: charListState.offset
-    //   })
-    // }
-
-    this.onRequest(); // при первом вызове формируется компонент и onRequest вызываем без передачи аргумента оффсета
-    // window.addEventListener('scroll', this.onLoadByScroll);
-  }
-
-  // componentWillUnmount() {
-  //   window.removeEventListener('scroll', this.onLoadByScroll);
-  // }
-
-  // onLoadByScroll = () => {
-  //   if (
-  //     window.innerHeight + document.documentElement.scrollTop >=
-  //     document.documentElement.offsetHeight - 150
-  //   ) {
-  //     this.onRequest(this.state.offset);
-  //   }
-  // };
-
-  onRequest = offset => {
-    this.onCharListLoading();
-    this.marvelService
-      .getAllCharacters(offset) // здесь если не передать аргуемент подставится null и метод возьмет оффсет по умолчанию в getAllCharacters
-      .then(this.onCharListLoaded) // сюда приходит ответ с сервера с 9ю персонажами и подставляется аргументом в onCharListLoaded
-      .catch(this.onError);
+  const onRequest = offset => {
+    onCharListLoading();
+    marvelService.getAllCharacters(offset) // здесь если не передать аргуемент подставится null и метод возьмет оффсет по умолчанию в getAllCharacters
+      .then(onCharListLoaded) // сюда приходит ответ с сервера с 9ю персонажами и подставляется аргументом в onCharListLoaded
+      .catch(onError);
   };
 
   // доп. метод, которы говорит о том что запустился запрос и что-то там грузится
-  onCharListLoading = () => {
-    this.setState({
-      newItemLoading: true,
-    });
+  const onCharListLoading = () => {
+    setNewItemLoading(true);
   };
 
-  onCharListLoaded = newCharList => {
-    // здесь аргуемнтом ответ сервера с 9* новыми загруженными персонажами
+  // здесь аргумент ответ сервера с 9* новыми загруженными персонажами
+  const onCharListLoaded = newCharList => {
     let ended = false;
     if (newCharList.length < 9) {
       ended = true;
     }
 
-    this.setState(({charList, offset}) => ({
-      // здесь достаем текущий стэйт
-      charList: [...charList, ...newCharList], // соединяем старый массив с персонажами с новым и пилим в стэйт
-      loading: false,
-      newItemLoading: false,
-      offset: offset + 9,
-      charEnded: ended,
-    }));
+    setCharList(charList => [...charList, ...newCharList]); // соединяем старый массив с персонажами с новым и пилим в стэйт
+    setLoading(false);
+    setNewItemLoading(false);
+    setOffset(offset => offset + 9);
+    setCharEnded(ended);
   };
 
-  onError = () => {
-    this.setState({
-      loading: false,
-      error: true,
-    });
+  const onError = () => {
+    setLoading(false);
+    setError(true);
   };
 
-  itemRefs = [];
+  const itemRefs = useRef([]);  // создаем массив с рефами, он будет лежать в itemRefs.current
 
-  setRef = ref => {
-    this.itemRefs.push(ref);
-  };
+  // эта функция сработает нормально в классах, а в функц. комп. push с рефами может дать ошибку. см. нужную функцию в назначении рефов
+  // setRef = ref => {
+  //   this.itemRefs.push(ref);
+  // };
 
-  focusOnItem = id => {
-    this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-    this.itemRefs[id].classList.add('char__item_selected');
-    this.itemRefs[id].focus();
+  const focusOnItem = id => {
+    itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));  
+    itemRefs.current[id].classList.add('char__item_selected');
+    itemRefs.current[id].focus();
   };
 
   // Этот метод создан для оптимизации, чтобы не помещать такую конструкцию в метод render
-  renderItems = arr => {
+  function renderItems(arr) {
     const items = arr.map((item, i) => {
       let imgStyle = 'char__item';
       if (
@@ -118,15 +81,15 @@ class CharList extends Component {
           className={imgStyle}
           key={item.id}
           tabIndex={0}
-          ref={this.setRef}
+          ref={elem => itemRefs.current[i] = elem}  // последовательно формируем массив с рефами, elem - ссылка на элемент в DOM
           onClick={() => {
-            this.props.onCharSelected(item.id);
-            this.focusOnItem(i);
+            props.onCharSelected(item.id);
+            focusOnItem(i);
           }}
           onKeyDown={e => {
             if (e.key === 'Enter' || e.key === ' ') {
-              this.props.onCharSelected(item.id);
-              this.focusOnItem(i);
+              props.onCharSelected(item.id);
+              focusOnItem(i);
             }
           }}>
           <img
@@ -138,36 +101,33 @@ class CharList extends Component {
       );
     });
 
-    // А эта конструкция вынесена для центровки спиннера/ошибки (?)
+    // А эта конструкция вынесена для центровки спиннера/ошибки 
     return <ul className="char__grid">{items}</ul>;
   };
 
-  render() {
-    const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+  const items = renderItems(charList);
 
-    const items = this.renderItems(charList);
+  const spinner = loading ? <Spinner /> : null;
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const content = !(loading || error) ? items : null;
 
-    const spinner = loading ? <Spinner /> : null;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const content = !(loading || error) ? items : null;
-
-    return (
-      <div className="char__list">
-        {spinner}
-        {errorMessage}
-        {content}
-        <button
-          className="button button__main button__long"
-          disabled={newItemLoading} //  если newItemLoading true кпонка блокируется
-          style={{display: charEnded ? 'none' : 'block'}} // если персонажи закончились скрываем кнопку
-          onClick={() => this.onRequest(offset)}>
-          <div className="inner">load more</div>
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="char__list">
+      {spinner}
+      {errorMessage}
+      {content}
+      <button
+        className="button button__main button__long"
+        disabled={newItemLoading} //  если newItemLoading true кпонка блокируется
+        style={{display: charEnded ? 'none' : 'block'}} // если персонажи закончились скрываем кнопку
+        onClick={() => onRequest(offset)}>
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
 }
 
+// Этот синтаксис будет нормально работать в функц.комп. при условии что ниже export default ( а не export const CharList = (props) => {...} )
 CharList.propTypes = {
   onCharSelected: PropTypes.func,
 };
