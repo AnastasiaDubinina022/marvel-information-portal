f7ab03c2fc613441714b6e0e8128f80d4b2c63ee
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2025-01-28 16:47:50 +0300

    Minor changes in index.js.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+// alt.variant
+// import {createRoot} from 'react-dom/client';
 import App from './components/app/App';
 
 import './styles/style.scss';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    // <React.StrictMode>
+      <App />
+    // </React.StrictMode>
+  );
+
+// alt. variant React.18
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// синтаксис 17й версии (так же импорт ReactDOM from 'react-dom';):
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
